a2bc217237483426cb6f1c2578f13c34e4789353
Esteban Torres <me@estebantorr.es>
2013-07-31 08:29:25 -0600

    New header file that will include all the UICategories fixes.
    Signed-off-by: Esteban Torres <me@estebantorr.es>

---
+++ ETFramework/UI/Categories/ETUICategories.h
@@ -0,0 +1,18 @@
+//
+//  ETUICategories.h
+//  ETFramework
+//
+//  Created by Esteban Torres on 7/31/13.
+//  Copyright (c) 2013 Esteban Torres. All rights reserved.
+//
+
+#ifndef ETFrameworkDemo_ETUICategories_h
+#define ETFrameworkDemo_ETUICategories_h
+
+#import "UITextField+EmailValidation.h"
+#import "UITextField+VisualHelpers.h"
+#import "UIView+KeyboardHelpers.h"
+#import "UIViewController+TextFieldDelegateHelpers.h"
+#import "UIViewController+ETFramework.h"
+
+#endif
