8d5536584cc739b3e2285c886bb7b94a2eea7e93
Esteban Torres <me@estebantorr.es>
2013-07-30 21:26:24 -0600

    Added some resources scripts (for build phases to auto-increment build version and another to mark important keywords as warnings [TODO, FIXME, ???, !!!]
    Signed-off-by: Esteban Torres <me@estebantorr.es>

---
+++ ETFramework/Resources/markImportantKeywords.sh
@@ -0,0 +1,2 @@
+KEYWORDS="TODO:|FIXME:|\?\?\?:|\!\!\!:"
+find "${SRCROOT}" \( -name "*.h" -or -name "*.m" \) -print0 | xargs -0 egrep --with-filename --line-number --only-matching "($KEYWORDS).*\$" | perl -p -e "s/($KEYWORDS)/ warning: \$1/"
