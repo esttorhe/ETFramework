3a0ff3155000215f6dc962f9b5f356a5104857ee
Esteban Torres <me@estebantorr.es>
2013-08-05 16:07:20 -0600

    Added detailed documentation (appledoc).
    Signed-off-by: Esteban Torres <me@estebantorr.es>

--- ETFramework/UI/Categories/UITextField+EmailValidation.h
+++ ETFramework/UI/Categories/UITextField+EmailValidation.h
@@ -8,8 +8,19 @@
 
 #import <UIKit/UIKit.h>
 
+/**
+ `UITextField (EmailValidation)` is a helper category that provides email validation methods.
+ */
 @interface UITextField (EmailValidation)
-
+/**------------------------------------------------------------------------------------------------------------------
+ * @name Email validation methods.
+ * ------------------------------------------------------------------------------------------------------------------
+ */
+/**
+ Returns wether or not the text on the `UITextField` instance matches an email pattern.
+ 
+ @return Returns `YES` if the text property of the `UITextField` instance matches a valid email pattern, `NO` otherwise.
+ */
 - (BOOL) isValidEmailAddress;
 
 @end
--- ETFramework/UI/Categories/UITextField+VisualHelpers.h
+++ ETFramework/UI/Categories/UITextField+VisualHelpers.h
@@ -7,13 +7,41 @@
 //
 
 #import <UIKit/UIKit.h>
-
+/**
+ `UITextField (VisualHelpers)` is a helper category that provided methods to handle showing and hiding error messaging.
+ */
 @interface UITextField (VisualHelpers)
 
+/**------------------------------------------------------------------------------------------------------------------
+ * @name Error messaging helper properties.
+ * ------------------------------------------------------------------------------------------------------------------
+ */
+/**
+ The `NSString` object that will be displayed as an error message on the `UITextField` instance.
+ */
 @property (nonatomic, retain) NSString *errorMessage;
 
+/**------------------------------------------------------------------------------------------------------------------
+ * @name Error handling methods.
+ * ------------------------------------------------------------------------------------------------------------------
+ */
+/**
+ Shows a `[[UIColor redColor] colorWithAlphaComponent:0.7f]` border around the `UITextField` instance.
+ */
 - (void) showErrorBorder;
+
+/**
+ Hides the error border (if any) of the `UITextField` instance.
+ 
+ @see -showErrorBorder;
+ */
 - (void) hideErrorBorder;
+
+/**
+ Shows the error message associated to the `UITextField` instance on the top half part of it.
+ 
+ @see -errorMessage;
+ */
 - (void) showErrorMessage;
 
 @end
