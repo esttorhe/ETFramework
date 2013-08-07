87dbf679ac31df0d830c0671d093e8318303b815
Esteban Torres <me@estebantorr.es>
2013-08-06 20:00:35 -0600

    Removed unnecessary code.
    Signed-off-by: Esteban Torres <me@estebantorr.es>

--- ETFramework/UI/Categories/UIViewController+TextFieldDelegateHelpers.m
+++ ETFramework/UI/Categories/UIViewController+TextFieldDelegateHelpers.m
@@ -8,41 +8,8 @@
 
 #import "UIViewController+TextFieldDelegateHelpers.h"
 #import "UIView+KeyboardHelpers.h"
-#import <objc/runtime.h>
-
-static char const * const TitleLabelKey = "TitleLabelKey";
-static char const * const LeftButtonKey = "LeftButtonKey";
-static char const * const RightButtonKey = "RightButtonKey";
 
 @implementation UIViewController (TextFieldDelegateHelpers)
-
-#pragma Properties
-
-- (id) viewTitleLabel {
-    return objc_getAssociatedObject(self, TitleLabelKey);
-}
-
-- (void)setViewTitleLabel:(UILabel *)viewTitleLabel{
-    objc_setAssociatedObject(self, TitleLabelKey, viewTitleLabel, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-}
-
-- (id) leftButton {
-    return objc_getAssociatedObject(self, LeftButtonKey);
-}
-
-- (void) setLeftButton:(UIButton *)leftButton
-{
-    objc_setAssociatedObject(self, LeftButtonKey, leftButton, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-}
-
-- (id) rightButton {
-    return objc_getAssociatedObject(self, RightButtonKey);
-}
-
-- (void) setRightButton:(UIButton *)rightButton
-{
-    objc_setAssociatedObject(self, RightButtonKey, rightButton, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-}
 
 #pragma mark - UITextFieldDelegate implementation
 
@@ -66,13 +33,6 @@ static char const * const RightButtonKey = "RightButtonKey";
     for (id view in [self.view subviews]) {
         [self.view cleanTextField:view];
     }
-}
-
-- (void) dealloc
-{
-    self.rightButton = nil;
-    self.leftButton = nil;
-    self.viewTitleLabel = nil;
 }
 
 @end
