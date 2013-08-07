84c15ef31b94295842cfc8c08fb85e9a39afd00f
Esteban Torres <me@estebantorr.es>
2013-08-06 19:59:55 -0600

    Added internal documentation that matches appledoc.
    Signed-off-by: Esteban Torres <me@estebantorr.es>

--- ETFramework/UI/Categories/UIViewController+ETFramework.h
+++ ETFramework/UI/Categories/UIViewController+ETFramework.h
@@ -7,9 +7,18 @@
 //
 
 #import <UIKit/UIKit.h>
-
+/**
+ `UIViewController (ETFramework)` is a helper category that adds some helper methods to `UIViewController` to handle and easily facilitate the use of this framework.
+ */
 @interface UIViewController (ETFramework)
 
+/**--------------------------------------------
+ * @name Initialisation methods
+ *---------------------------------------------
+ */
+/**
+ Adds a predefined `UITapGestureRecognizer` with `-numberOfTouchesRequired:`1 and `-numberOfTapsRequired:`1 that will internally call `-resignTextFields` and will dismiss the keyboard.
+ */
 - (void) initializeViewController;
 
 @end
