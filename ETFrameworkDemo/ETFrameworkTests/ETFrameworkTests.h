63fa328fc8e4033d5af01f659c0b9f083611e23f
Esteban Torres <me@estebantorr.es>
2013-08-12 17:42:36 -0600

    Added placeholder tests for ETFrameworkDemo
    Signed-off-by: Esteban Torres <me@estebantorr.es>

---
+++ ETFrameworkDemo/ETFrameworkTests/ETFrameworkTests.h
@@ -0,0 +1,13 @@
+//
+//  ETFrameworkTests.h
+//  ETFrameworkTests
+//
+//  Created by Esteban Torres on 8/12/13.
+//  Copyright (c) 2013 estebantorres. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ETFrameworkTests : SenTestCase
+
+@end
---
+++ ETFrameworkDemo/ETFrameworkTests/ETFrameworkTests.m
@@ -0,0 +1,32 @@
+//
+//  ETFrameworkTests.m
+//  ETFrameworkTests
+//
+//  Created by Esteban Torres on 8/12/13.
+//  Copyright (c) 2013 estebantorres. All rights reserved.
+//
+
+#import "ETFrameworkTests.h"
+
+@implementation ETFrameworkTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ETFrameworkTests");
+}
+
+@end
