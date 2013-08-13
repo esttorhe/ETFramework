77fe0a8d7f1bae359d268d1ea7d70c038493437d
Esteban Torres <me@estebantorr.es>
2013-08-13 10:20:29 -0600

    Added new documentation specification to point out for required parameters.
    Signed-off-by: Esteban Torres <me@estebantorr.es>

--- ETFramework/Core/ETArray.h
+++ ETFramework/Core/ETArray.h
@@ -13,27 +13,28 @@
 
 @interface NSArray (ETArray)
 
-/**------------------------------------------------------------------------------------------------------------------
- * @name Sort Descriptors
- * ------------------------------------------------------------------------------------------------------------------
- */
-/**
- Creates a sort descriptor for the column provided in ascending order.
+/// ------------------------------------------------------------------------------------------------------------------
+/// @name Sort Descriptors
+/// ------------------------------------------------------------------------------------------------------------------
+
+/** Creates a sort descriptor for the column provided in ascending order.
  
- @param column The column name that will be used as the sort descriptor.
+ This method calls internally `-sortDescriptor:ascending:` with `YES` as the `ascending` parameter.
  
+ @param column **[Required]** The column name that will be used as the sort descriptor.
  @return An array containing the newly created sort descriptor with the column provided and ascending order.
+ @see -sortDescriptor:ascending:
+ @warning **`column`** parameter is required; if not provided will return `nil` unless `ETFRAMEWORK_RAISE_ASSERTS` is defined in which case will raise an exception.
  */
 + (NSArray*) sortDescriptor:(NSString *)column;
 
 /**
  Creates a sort descriptor for the column provided with the provided order.
  
- @param column The column name that will be used as the sort descriptor.
- 
+ @param column **[Required]** The column name that will be used as the sort descriptor.
  @param ascending Flag indicating wether or not the sort description should be created ascending or descending.
- 
  @return An array containing the newly created sort descriptor with the column provided and provided ascending order.
+ @warning **`column`** parameter is required; if not provided will return `nil` unless `ETFRAMEWORK_RAISE_ASSERTS` is defined in which case will raise an exception.
  */
 + (NSArray*) sortDescriptor:(NSString *)column ascending:(BOOL)ascending;
 
@@ -52,20 +53,22 @@
 /**
  Sorts the NSSet and returns it sorted ascending.
  
- @param field The field used as the key to sort the NSSet.
+ This methods calls internally `-sortByField:ascending:` with `YES` as the `ascending` parameter.
  
+ @param field **[Required]** The field used as the key to sort the NSSet.
  @return An array build from the NSSet objects sorted by the field in ascending order.
+ @see -sortByField:ascending:
+ @warning **`field`** parameter is required; if not provided will return `nil` unless `ETFRAMEWORK_RAISE_ASSERTS` is defined in which case will raise an exception.
  */
 - (NSArray*) sortByField:(NSString *)field;
 
 /**
  Sorts the NSSet and returns it sorted in the direction provided.
  
- @param field The field used as the key to sort the NSSet.
- 
+ @param field **[Required]** The field used as the key to sort the NSSet.
  @param ascending Flag indicating wether or not the sorting should be done ascending or descending.
- 
  @return An array build from the NSSet objects sorted by the field in the provided ascending order.
+ @warning **`field`** parameter is required; if not provided will return `nil` unless `ETFRAMEWORK_RAISE_ASSERTS` is defined in which case will raise an exception.
  */
 - (NSArray*) sortByField:(NSString *)field ascending:(BOOL)ascending;
 
